645062c28afa0c9544a5f96a160a8e45439d3456
Gwilherm Baudic <gbaudic@users.noreply.github.com>
2018-04-05 10:56:02 +0200

    Shopping list without data persistence

--- laChouetteAppli/src/pages/home/home.ts
+++ laChouetteAppli/src/pages/home/home.ts
@@ -1,18 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+// import { Dialogs } from '@ionic-native/dialogs';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items: ShoppingItems[];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController/*,
+      public dialogs: Dialogs*/) {
 
   }
   
-  addItem() {
+  addItem(): void {
+    // Open dialog and add to array
+    /*this.dialogs.prompt('Ajouter item', '', ['OK', 'Annuler'], '').then(
+      theResult => {    
+          if ((theResult.buttonIndex == 1) && (theResult.input1 !== '')) {
+            this.items.push({ name: theResult.input1, bought: false });                      
+          }
+        });*/
+  }
   
+  deleteItem(item: ShoppingItem): void {
+    item.bought = true;
+    let index = this.items.indexOf(item);   
+    if (index > -1) {
+      this.items.splice(index, 1);
+    }
+  }
+  
+  markAsDone(item: ShoppingItem): void {
+    item.bought = true;
   }
 
 }
