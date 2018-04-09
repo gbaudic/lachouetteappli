b4018275c0e06cc4f6e252448b2c5a205f1e709a
gbaudic <gbaudic@users.noreply.github.com>
2018-04-09 18:56:00 +0200

    More calculator implementation

--- laChouetteAppli/src/pages/calculator/calculator.ts
+++ laChouetteAppli/src/pages/calculator/calculator.ts
@@ -14,6 +14,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'calculator.html',
 })
 export class CalculatorPage {
+  // Note: all amounts are expressed in cents to get around potential rounding issues
   amounts: Array<[number, number]> = [[1,0],[2,0],[5,0],[10,0],[20,0],[50,0],
 									  [100,0],[200,0],[500,0],[1000,0],[2000,0],[5000,0],[10000,0],[20000,0],[50000,0]];
   total = 0;
@@ -25,8 +26,17 @@ export class CalculatorPage {
     console.log('ionViewDidLoad CalculatorPage');
   }
   
-  reset() {
+  compute() {
+    this.total = 0;
+	for(let el of this.amounts) {
+	  this.total += el[0]*el[1];
+	}
+  }
   
+  reset() {
+    for(let el of this.amounts) {
+	  el[1] = 0;
+	}
   }
 
 }
